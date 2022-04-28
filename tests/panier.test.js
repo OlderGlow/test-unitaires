04986a14884bd2b99a82b70c5752327359ec5a8a
Julien PICQUET <julienpicquet1@gmail.com>
2022-04-28 13:27:56 +0200

    add should

--- tests/panier.test.js
+++ tests/panier.test.js
@@ -3,23 +3,23 @@ import should from "should";
 describe('Test de la fonction calculPanier', () => {
     it('calculPanier doit retourner un nombre', () => {
         debugger;
-        calculPanier().should.be.a.Number();
+        should(calculPanier()).be.a.Number();
     }
     );
 });
 describe('Test de la fonction calculTVA', () => {
     it('calculTVA doit retourner un nombre', () => {
-        calculerTVA(article1.prixHT, article1.tauxTVA).should.be.a.Number();
-        calculerTVA(article2.prixHT, article2.tauxTVA).should.be.a.Number();
-        calculerTVA(article3.prixHT, article3.tauxTVA).should.be.a.Number();
+       should(calculerTVA(article1.prixHT, article1.tauxTVA)).be.a.Number();
+       should(calculerTVA(article2.prixHT, article2.tauxTVA)).be.a.Number();
+       should(calculerTVA(article3.prixHT, article3.tauxTVA)).be.a.Number();
     }
     );
 });
 describe('Test de la fonction calculFraisPort', () => {
     it('calculFraisPort doit retourner un nombre', () => {
-        calculerFraisPort(article1.prixHT).should.be.a.Number();
-        calculerFraisPort(article2.prixHT).should.be.a.Number();
-        calculerFraisPort(article3.prixHT).should.be.a.Number();
+        should(calculerFraisPort(10)).be.a.Number();
+        should(calculerFraisPort(23)).be.a.Number();
+        should(calculerFraisPort(calculPanier())).be.a.Number();
     }
     );
 }
