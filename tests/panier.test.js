08e1f26d1b69b645df04a5b5022c8fbb32045033
Julien PICQUET <julienpicquet1@gmail.com>
2022-04-28 12:54:22 +0200

    add multiples tests

--- tests/panier.test.js
+++ tests/panier.test.js
@@ -10,12 +10,16 @@ describe('Test de la fonction calculPanier', () => {
 describe('Test de la fonction calculTVA', () => {
     it('calculTVA doit retourner un nombre', () => {
         calculerTVA(article1.prixHT, article1.tauxTVA).should.be.a.Number();
+        calculerTVA(article2.prixHT, article2.tauxTVA).should.be.a.Number();
+        calculerTVA(article3.prixHT, article3.tauxTVA).should.be.a.Number();
     }
     );
 });
 describe('Test de la fonction calculFraisPort', () => {
     it('calculFraisPort doit retourner un nombre', () => {
         calculerFraisPort(article1.prixHT).should.be.a.Number();
+        calculerFraisPort(article2.prixHT).should.be.a.Number();
+        calculerFraisPort(article3.prixHT).should.be.a.Number();
     }
     );
 }
