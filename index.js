a5a4be722f7cf00255a087ed387a6ebed03b86bb
Julien PICQUET <julienpicquet1@gmail.com>
2022-04-28 11:55:30 +0200

    first commit

--- index.js
+++ index.js
@@ -21,12 +21,24 @@ const calculerTVA = (prixHT, tauxTVA) => {
     if(isNaN(tauxTVA) || isNaN(prixHT)) {
         return 'Erreur, veuillez rentrer des nombres';
     }
+    if(tauxTVA > 100) {
+        return 'Erreur, le taux de TVA est supérieur à 100%';
+    }
+    if(tauxTVA < 0) {
+        return 'Erreur, le taux de TVA est inférieur à 0%';
+    }
+    if(prixHT < 0) {
+        return 'Erreur, le prix HT est inférieur à 0';
+    }
     return prixHT * tauxTVA / 100;
 };
 
 const calculerFraisPort = (prixTotal) => {
     if(isNaN(prixTotal)) {
         return 'Erreur, veuillez rentrer un nombre';
+    }
+    if(prixTotal < 0) {
+        return 'Erreur, le prix total est inférieur à 0';
     }
     if (prixTotal < 50) {
         return 5;
@@ -40,6 +52,21 @@ const calculPanier = () => {
     let prixTotal = 0;
     const panier = [article1, article2, article3];
     panier.map(article => {
+        if(isNaN(article.prixHT) || isNaN(article.tauxTVA) || isNaN(article.quantite)) {
+            return 'Erreur, veuillez rentrer des nombres';
+        }
+        if(article.prixHT < 0) {
+            return 'Erreur, le prix HT est inférieur à 0';
+        }
+        if(article.tauxTVA < 0) {
+            return 'Erreur, le taux de TVA est inférieur à 0%';
+        }
+        if(article.tauxTVA > 100) {
+            return 'Erreur, le taux de TVA est supérieur à 100%';
+        }
+        if(article.quantite < 0) {
+            return 'Erreur, la quantité est inférieur à 0';
+        }
         prixTotal += (article.prixHT + calculerTVA(article.prixHT, article.tauxTVA)) * article.quantite;
     });
     console.log(`Le prix total du panier est de ${prixTotal} €`);
